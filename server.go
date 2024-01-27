47d0c5b0312e3593d58a04e62411f293ad6782b6
Gabriel Castillo <sk8.andrade@gmail.com>
2024-01-27 20:52:03 +0100

    Add models

--- server.go
+++ server.go
@@ -9,10 +9,6 @@ import (
 	"github.com/gofiber/fiber/v2/middleware/requestid"
 )
 
-type Campaing struct {
-	CampaingId string `json:"campaingId"`
-}
-
 func startServer() {
 
 	app := fiber.New()
@@ -35,29 +31,11 @@ func setupMiddleWares(app *fiber.App) {
 }
 
 func setupRoutes(app *fiber.App) {
-	app.Get("/helloLosPibes", handleHelloLosPibes)
 	app.Get("/ping", handlePing)
 	app.Get("/store", handleStoreRedirect)
-	app.Post("/campaing", handleCampaingEvent)
-}
-
-func handleCampaingEvent(c *fiber.Ctx) error {
-
-	campaing := new(Campaing)
-	err := c.BodyParser(campaing)
-
-	if err != nil {
-		c.Status(fiber.StatusBadRequest).SendString(err.Error())
-		return err
-	}
-
-	log.Default().Println(campaing.CampaingId)
-
-	return c.Status(fiber.StatusOK).JSON(campaing)
 }
 
 func handlePing(c *fiber.Ctx) error {
-	// log.Default().Printf("Headers: %v", c.GetReqHeaders())
 	headers := c.GetReqHeaders()
 	ip := headers["X-Forwarded-For"]
 	log.Default().Printf("%v", ip)
@@ -65,10 +43,8 @@ func handlePing(c *fiber.Ctx) error {
 }
 
 func handleStoreRedirect(c *fiber.Ctx) error {
-	log.Default().Printf("Headers: %v", c.GetReqHeaders())
+	headers := c.GetReqHeaders()
+	ip := headers["X-Forwarded-For"]
+	log.Default().Printf("%v", ip)
 	return c.Redirect("https://apps.apple.com/es/app/group-task-manager/id6444686521")
-}
-
-func handleHelloLosPibes(c *fiber.Ctx) error {
-	return c.SendString("Hola los pibes!")
 }
--- storage.go
+++ storage.go
@@ -4,36 +4,62 @@ import (
 	"context"
 	"encoding/json"
 	"log"
-	"os"
+	"time"
 
-	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+type Campaign struct {
+	Id           string    `json:"_id"`
+	Name         string    `json:"name"`
+	Source       string    `json:"source"`
+	AppStoreLink string    `json:"appStoreLink"`
+	CreatedAt    time.Time `json:"createdAt"`
+	UpdatedAt    time.Time `json:"updatedAt"`
+}
+
+type Event struct {
+	Id         string    `json:"_id"`
+	Name       string    `json:"name"`
+	CampaignId string    `json:"campaignId"`
+	CreatedAt  time.Time `json:"createdAt"`
+	UpdateAt   time.Time `json:"updateAt"`
+	UserId     string    `json:"userId"`
+}
+
+type AppUser struct {
+	Id         string    `json:"_id"`
+	CampaignId string    `json:"campaignId"`
+	UserIp     string    `json:"userIp"`
+	Email      string    `json:"email"`
+	CreatedAt  time.Time `json:"createdAt"`
+	UpdatedAt  time.Time `json:"updatedAt"`
+}
+
 func setupDatabase() {
 
-	var uri string
+	uri := "mongodb+srv://adwizard-admin:3HOOQRUQLl7RQpQk@adwizardcluster.uuyu7mt.mongodb.net/admin?authSource=admin&replicaSet=atlas-4e258e-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true"
 
-	if os.Getenv("RAILWAY") == "TRUE" {
+	// if os.Getenv("RAILWAY") == "TRUE" {
 
-		uri = os.Getenv("MONGO_PRIVATE_URL")
-		if uri == "" {
-			log.Fatal("You must set your 'MONGO_PRIVATE_URL'")
-		}
-	} else {
+	// 	uri = os.Getenv("MONGO_PRIVATE_URL")
+	// 	if uri == "" {
+	// 		log.Fatal("You must set your 'MONGO_PRIVATE_URL'")
+	// 	}
+	// } else {
 
-		err := godotenv.Load(".env")
-		if err != nil {
-			log.Fatalf("Error loading environment variables file")
-		}
+	// 	err := godotenv.Load(".env")
+	// 	if err != nil {
+	// 		log.Fatalf("Error loading environment variables file")
+	// 	}
 
-		uri = os.Getenv("MONGO_DB_URI_FROM_LOCAL")
-		if uri == "" {
-			log.Fatal("You must set your 'MONGO_PRIVATE_URL'")
-		}
-	}
+	// 	uri = os.Getenv("MONGO_DB_URI_FROM_LOCAL")
+	// 	if uri == "" {
+	// 		log.Fatal("You must set your 'MONGO_PRIVATE_URL'")
+	// 	}
+	// }
 
 	// Get client
 	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
@@ -43,14 +69,14 @@ func setupDatabase() {
 
 	// Test retreiving some data
 	ctx := context.Background()
-	collection := client.Database("test").Collection("events")
+	collection := client.Database("production").Collection("Campaign")
 
 	cursor, err := collection.Find(ctx, bson.M{})
 	if err != nil {
 		log.Panic(err)
 	}
 
-	var campaings []Campaing
+	var campaings []Campaign
 
 	err = cursor.All(ctx, &campaings)
 	if err != nil {
