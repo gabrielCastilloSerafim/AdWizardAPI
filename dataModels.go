23468d76ee3789c76514d1e955b45fb8dec15a4c
Gabriel Castillo <sk8.andrade@gmail.com>
2024-01-28 18:38:36 +0100

    Add missing json tags on data models

--- dataModels.go
+++ dataModels.go
@@ -7,28 +7,28 @@ import (
 )
 
 type Campaign struct {
-	Id           primitive.ObjectID `bson:"_id,omitempty"`
+	Id           primitive.ObjectID `bson:"_id,omitempty" json:"_id"`
 	Name         string             `bson:"name" json:"name"`
 	Source       string             `bson:"source" json:"source"`
 	AppStoreLink string             `bson:"appStoreLink" json:"appStoreLink"`
-	CreatedAt    time.Time          `bson:"createdAt"`
-	UpdatedAt    time.Time          `bson:"updatedAt"`
+	CreatedAt    time.Time          `bson:"createdAt" json:"createdAt"`
+	UpdatedAt    time.Time          `bson:"updatedAt" json:"updatedAt"`
 }
 
 type Event struct {
-	Id         primitive.ObjectID `bson:"_id,omitempty"`
+	Id         primitive.ObjectID `bson:"_id,omitempty" json:"_id"`
 	Name       string             `bson:"name" json:"name"`
 	CampaignId string             `bson:"campaignId" json:"campaignId"`
-	CreatedAt  time.Time          `bson:"createdAt"`
-	UpdateAt   time.Time          `bson:"updateAt"`
+	CreatedAt  time.Time          `bson:"createdAt" json:"createdAt"`
+	UpdateAt   time.Time          `bson:"updateAt" json:"updateAt"`
 	UserId     string             `bson:"userId" json:"userId"`
 }
 
 type AppUser struct {
-	Id         primitive.ObjectID `bson:"_id,omitempty"`
-	CampaignId string             `bson:"campaignId"`
-	UserIp     string             `bson:"userIp"`
-	Email      string             `bson:"email"`
-	CreatedAt  time.Time          `bson:"createdAt"`
-	UpdatedAt  time.Time          `bson:"updatedAt"`
+	Id         primitive.ObjectID `bson:"_id,omitempty" json:"_id"`
+	CampaignId string             `bson:"campaignId" json:"campaignId"`
+	UserIp     string             `bson:"userIp" json:"userIp"`
+	Email      string             `bson:"email" json:"email"`
+	CreatedAt  time.Time          `bson:"createdAt" json:"createdAt"`
+	UpdatedAt  time.Time          `bson:"updatedAt" json:"updatedAt"`
 }
